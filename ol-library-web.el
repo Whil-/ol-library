761cc8858b7c9f8de7a39013cdc05c09ef359eae
Gustav Wikström <gustav@whil.se>
2020-05-23 20:16:04 +0200

    Fix web-links

--- ol-library-web.el
+++ ol-library-web.el
@@ -1,15 +1,17 @@
 ;;; ol-library-web-*
 (require 'ol-library)
 
-(defcustom ol-library-web-dir "~/webarchive"
+(defcustom ol-library-web-dir "~/webs"
   "The path to be used for web-links."
-  :group 'ol-library-web
+  :group 'ol-library
   :type 'directory
   :safe t)
 
-(defcustom ol-library-web-regexp (concat "\\`[^.].*\\." (regexp-opt (list "html" "pdf" "zip")) "\\'")
+(defcustom ol-library-web-regexp (concat "\\`[^.].*\\."
+                                         (regexp-opt (list "html" "pdf" "zip"))
+                                         "\\'")
   "Regular expression to match files for `ol-library-web-store-link'"
-  :group 'ol-library-web
+  :group 'ol-library
   :type 'regexp
   :safe t)
 
@@ -19,31 +21,32 @@
 The first function in this list defines the preferred function
 which will be used when creating new attachment folders. All
 functions of this list will be tried when looking for webs."
-  :group 'ol-library-web
+  :group 'ol-library
   :type '(repeat (function :tag "Function with webname as input"))
   :safe t)
 
 (defun ol-library-web--collect ()
     "Create collection of webs."
-    (ol-library-collect ol-library-web-dir ol-library-web-regexp))
+    (ol-library-collect ol-library-web-dir
+                        ol-library-web-regexp))
 
 (defun ol-library-web--expand (web)
-  "Expand a web into a path.
+  "Expand an web into a path.
 Based on `ol-library-web-to-path-function-list' and
 `ol-library-web-dir'."
   (ol-library-dir-from-library-object web
-                                       org-link-took-to-path-function-list
+                                       ol-library-web-to-path-function-list
                                        ol-library-web-dir))
 
 (defun ol-library-web-follow (web arg)
   "Open WEB attachment.
 See `org-open-file' for details about ARG."
-  (org-link-open-as-file (ol-library-web-expand web) arg))
+  (org-link-open-as-file (ol-library-web--expand web) arg))
 
 (defun ol-library-web-complete-link ()
   "Advise the user with the available files in the attachment directory."
   (if (file-exists-p ol-library-web-dir)
-      (concat "web:" (completing-read "Web: " (ol-library-web-collect)))
+      (concat "web:" (completing-read "Web: " (ol-library-web--collect)))
     (error "No web directory exist")))
 
 ;;; org-link-set-parameters
